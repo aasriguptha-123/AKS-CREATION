c5bb7c24d60606375aea5d0ae2740462ec7aaa04
aasriguptha-123 <aasri.guptha@gmail.com>
2026-04-27 19:00:23 +0530

    Update main.tf

--- ACR/main.tf
+++ ACR/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
-  resource_group_name = azurerm_resource_group.acr_rg.name
-  location            = azurerm_resource_group.acr_rg.location
+  resource_group_name = "aksdemocluster-rg"
+  location            = "australiaeast"
   sku                 = var.acr_sku
 
   admin_enabled       = false
