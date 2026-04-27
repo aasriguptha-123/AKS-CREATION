3372f482484233e34d3412e4449cfcccce32bbc9
aasriguptha-123 <aasri.guptha@gmail.com>
2026-04-27 19:08:21 +0530

    Update main.tf

--- ACR/main.tf
+++ ACR/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = false
   }
  resource "azurerm_role_assignment" "acr_pull"{
-  principal_id = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].object_id
+  principal_id = "e67b18f1-5cd6-40f9-977f-aa530c9a60c5"
   role_definition_name = "AcrPull"
   scope = azurerm_container_registry.acr.id
  }
